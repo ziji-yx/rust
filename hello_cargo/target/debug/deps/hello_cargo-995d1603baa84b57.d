e0026428c2bc1a89cf82e998ffbd60557d10e84c
ziji-yx <1271948664@qq.com>
2026-09-05 02:57:42 +0800

    第二次提交：Cargo工具的使用

---
+++ hello_cargo/target/debug/deps/hello_cargo-995d1603baa84b57.d
@@ -0,0 +1,5 @@
+C:\Users\39232\Desktop\rust\hello_cargo\target\debug\deps\hello_cargo-995d1603baa84b57.d: src\main.rs
+
+C:\Users\39232\Desktop\rust\hello_cargo\target\debug\deps\libhello_cargo-995d1603baa84b57.rmeta: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-d5e6779a2942750a.d
@@ -0,0 +1,5 @@
+C:\Users\39232\Desktop\rust\hello_cargo\target\debug\deps\hello_cargo-d5e6779a2942750a.d: src\main.rs
+
+C:\Users\39232\Desktop\rust\hello_cargo\target\debug\deps\libhello_cargo-d5e6779a2942750a.rmeta: src\main.rs
+
+src\main.rs:
